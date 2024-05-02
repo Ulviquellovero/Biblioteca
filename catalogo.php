fd585fd59c68772419a99a377690e4d0ad01489a
Ulviquellovero <ulivi.samuel@volta.ts.it>
2024-05-02 18:55:50 +0200

    Titoli Catalogo

--- catalogo.php
+++ catalogo.php
@@ -2,6 +2,7 @@
     <head>
         <title>Catalogo</title>
         <link rel="icon" type="image/png" href="img/Sapienza.png">
+        <link rel="stylesheet" href="css/catalogo_style.css">
     </head>
 
     <body>
@@ -10,5 +11,32 @@
                 require_once("header.php");
             ?>
         </div>
+        <div id='visualizzazione'>
+            
+        </div>
     </body>
+
+    <script>
+        creaTabellaLibri();
+        function creaTabellaLibri()
+        {
+            const xhttp = new XMLHttpRequest();
+            xhttp.onload = function() {
+                var res = xhttp.responseText;
+                var j = JSON.parse(res);
+                var visualizzazione = document.getElementById("visualizzazione");
+                visualizzazione.innerHTML = "";
+
+                for(var i=0; i < j.Result.length; i++)
+                {
+                    var titolo = document.createElement("h1");
+                    titolo.className = "titoloLibro";
+                    titolo.textContent = j.Result[i].titolo;
+                    visualizzazione.appendChild(titolo);
+                }
+            }
+            xhttp.open("POST", "crea_catalogo_libri.php", true);
+            xhttp.send();
+        }
+    </script>
 </html>
---
+++ crea_catalogo_libri.php
@@ -0,0 +1,28 @@
+<?php
+    require_once("var_conn.php");
+    $sql = "SELECT idLibro, titolo, annoPubblicazione, nomeAutore, cognomeAutore, nomeCasaEditrice, disponibile
+            FROM tlibro";
+    $res = mysqli_query($con, $sql);
+    $numRigheReali = mysqli_num_rows($res);
+    $i = 0;
+    $resArr = null;
+    while($array = mysqli_fetch_array($res)) 
+    {
+		$row = array(
+					"id" => $array['idLibro'],
+					"titolo" => $array['titolo'],
+					"annoPub" => $array['annoPubblicazione'],
+					"nomeAutore" => $array['nomeAutore'],
+					"cognomeAutore" => $array['cognomeAutore'],
+					"nomeCasaEditrice" => $array['nomeCasaEditrice'],
+					"disponibile" => $array['disponibile']
+					);
+		$resArr[$i] = $row;
+		$i++;
+	}
+	$risFin = array(
+				"Result" => $resArr,
+				);
+	echo json_encode($risFin);
+    require_once("var_connclose.php");
+?>
