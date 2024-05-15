b0f1413a53086c5292aefb4b1eb33b4f7bad2d39
Ulviquellovero <ulivi.samuel@volta.ts.it>
2024-05-15 16:04:21 +0200

    Drttagli Prestito Volume FUnzionante

--- dettaglio_volume.php
+++ dettaglio_volume.php
@@ -45,6 +45,29 @@
                     "identificativoArmadio" => $arrayPosizione['identificativoArmadio'],
                     "identificativoStanza" => $arrayPosizione['identificativoStanza']
                     );
+
+                $idLibro = $array['idVolume'];
+                $sqlPrenotazione = "SELECT tpl.idUtente AS idUtente, tpl.data AS data, tu.nome AS nome, tu.cognome AS cognome, tu.codiceFiscale AS codiceFiscale, tpl.idPersonaleErogatore AS idPersonaleErogatore, tpl.idPersonaleConsegna AS idPersonaleConsegna FROM tprestitovolume tpl JOIN tutente tu ON tpl.idUtente = tu.idUtente WHERE tpl.idVolume = $idLibro";
+                $resPrenotazione = mysqli_query($con, $sqlPrenotazione);
+                if(mysqli_num_rows($resPrenotazione) != 0) 
+                {
+                    $arrayPrenotazione = mysqli_fetch_array($resPrenotazione);
+                    $nomeUtente = $arrayPrenotazione['nome'] . " " . $arrayPrenotazione['cognome'];
+                    $row["data"] = $arrayPrenotazione['data'];
+                    $row["nomeUtente"] = $nomeUtente;
+                    $row["codiceFiscale"] = $arrayPrenotazione['codiceFiscale'];
+                    $idPersonaleErogatore = $arrayPrenotazione['idPersonaleErogatore'];
+                    $sqlPersonaleErogatore = "SELECT nomeUtente FROM tpersonale WHERE idPersonale = $idPersonaleErogatore";
+                    $resPersonaleErogatore = mysqli_query($con, $sqlPersonaleErogatore);
+                    $arrayPersonaleErogatore = mysqli_fetch_array($resPersonaleErogatore);
+                    $row["nomeUtentePersonaleErogatore"] = $arrayPersonaleErogatore['nomeUtente'];
+                    $idPersonaleConsegna = $arrayPrenotazione['idPersonaleConsegna'];
+                    $sqlPersonaleConsegna = "SELECT nomeUtente FROM tpersonale WHERE idPersonale = $idPersonaleConsegna";
+                    $resPersonaleConsegna = mysqli_query($con, $sqlPersonaleConsegna);
+                    $arrayPersonaleConsegna = mysqli_fetch_array($resPersonaleConsegna);
+                    $row["nomeUtentePersonaleConsegna"] = $arrayPersonaleConsegna['nomeUtente'];
+                    $row["idUtente"] = $arrayPrenotazione['idUtente'];
+                }
             }
             else
             {
